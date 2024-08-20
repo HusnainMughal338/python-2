83ae21ca634dcc8880042a24cad6d497bc744f4a
HusnainMughal338 <mughalhusnain437@gmail.com>
2024-08-20 15:06:33 +0000

    init github

---
+++ ASSI03.py
@@ -0,0 +1,3 @@
+length_of_triangle:str = ("10cm")
+width_of_triangle:str = ("12cm")
+print("Area of Triangle",("length_of_triangle*width_of_triangle"))
---
+++ working.py
@@ -0,0 +1,4 @@
+#print(Calculate the BMI by using height meters and weight in kilograms) 
+weight:int = (65)
+height:int = (2)
+print("Body mass index is",weight/height**2)
