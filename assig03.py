084a7ae879c2140e900ff7ae807078d8eefd456d
HusnainMughal338 <mughalhusnain437@gmail.com>
2024-08-25 09:42:42 +0000

    init github

--- assig03.py
+++ assig03.py
@@ -1,30 +1,44 @@
 current_year:int = (2024)
 birth_year:int = (2005)
 print("The age of Husnain Mughal is",(current_year-birth_year))
+
+
 #print(Find the area of Triangle)
 length_of_triangle:int = (12)
 width_of_triangle:int = (10)
 print("Area of Tringle is",(length_of_triangle*width_of_triangle))
+
+
 #(Find the area of a circle)
 radius_of_a_circle:int = (3)
 area_of_a_circle:float = (3.14*(radius_of_a_circle)**2)
 print("The total area of cube is",(area_of_a_circle))
+
+
 #print(Find the area of a cube)
 surface_area:int = (6)
 area_of_a_cube:float = (6*(surface_area)**2)
 print("The total area of a cube is",(area_of_a_cube))
+
+
 #print(creat a program to convert tempratre to fahrenheit)
 temprature:int = (22)
 convert_to_fahrenhiet:float = ((1.8*temprature)+32)
 print("To convert temprature into fahrenhiet is",(convert_to_fahrenhiet))
+
+
 #print(Find the percentage of husnain matric result)
 obtain_marks:int = (966)
 total_marks:int = (1100)
 print("The percentage of Husnain marks is",(obtain_marks/total_marks)*100)
+
+
 #print(Find the volume of a cylinder)
 radius_of_cylinder:int = (10)
 hight_of_cylinder:int = (12)
 print("The volume of a cylinder is",3.14*(radius_of_cylinder)**2*hight_of_cylinder)
+
+
 #print(Calculate the BMI by using height meters and weight in kilograms) 
 weight:int = (65)
 height:int = (2)
