30306beb2feef0efb2364dcb00ed543e20d86729
HusnainMughal338 <mughalhusnain437@gmail.com>
2024-09-05 04:59:26 +0000

    past

--- assig05.py
+++ assig05.py
@@ -131,3 +131,41 @@ age = ageDetermine()
 
 
 print("\n||-----------------Question End------------------||\n")
+
+
+
+#Write a program to find month numbers.
+print("Question no #7 :Find the month name for the given number?\n")
+def month_name():    
+    month = int(input("Enter a month number: "))
+    match month:
+        case 1:
+            print("January")
+        case 2:
+            print("February")
+        case 3:
+            print("March")
+        case 4:
+            print("April")
+        case 5:
+            print("May")
+        case 6:
+            print("June")
+        case 7:
+            print("July")
+        case 8:
+            print("August")
+        case 9:
+            print("September")
+        case 10:
+            print("October")
+        case 11:
+            print("November")
+        case 12:
+            print("December")
+        case _:  
+            print("Invalid")
+    return month_name
+
+
+result = month_name()
